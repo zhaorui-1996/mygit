fbdbd0fe159fc1f2a678ff7b72f13ec9b83a9122
zhaorui-1996 <zhaorui@jhkj66.com>
2020-03-10 11:54:29 +0800

    修改提交

---
+++ helloword/src/Test/hellowomanTest.java
@@ -0,0 +1,7 @@
+package Test;
+
+public class hellowomanTest {
+	public static void main(String[] args){
+    	System.out.println("hello woman");
+    }
+}
--- helloword/src/Test/hellowordTest.java
+++ helloword/src/Test/hellowordTest.java
@@ -2,6 +2,6 @@ package Test;
 
 public class hellowordTest {
     public static void main(String[] args){
-    	System.out.println("ÄãºÃ£¬ÊÀ½ç");
+    	System.out.println("hello word");
     }
 }
